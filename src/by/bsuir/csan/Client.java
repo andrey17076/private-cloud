0d48cf830c58868a290524c0ca96bd5b5332c44e
Andrey Tatarenko <andrey17076@gmail.com>
2016-05-16 18:17:00 +0300

    Initial commit

---
+++ src/by/bsuir/csan/Client.java
@@ -0,0 +1,8 @@
+package by.bsuir.csan;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/by/bsuir/csan/Server.java
@@ -0,0 +1,8 @@
+package by.bsuir.csan;
+
+public class Server {
+
+    public static void main(String[] args) {
+
+    }
+}
